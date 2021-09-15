bc97e6471675779c02963b6b6fe5909561a945e4
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-09-15 18:21:55 -0400

    partial add task function

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -3,7 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
   
 	var calendar = new FullCalendar.Calendar(calendarEl, {
 		initialView: 'timeGrid',
-		dayCount: 1
+		dayCount: 1,
+
+		header: {
+			center: 'addEventButton'
+		},
+		
+		customButtons: {
+			addEventButton: {
+				text: 'Add Task',
+				click: function() {
+					var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+					var date = moment(dateStr);
+
+					if (date.isValid()) {
+						$('#calendar').FullCalendar('renderEvent', {
+						title: 'dynamic event',
+						start: date,
+						allDay: true
+						});
+						alert('Great. Now, update your database...');
+					} else {
+						alert('Invalid date.');
+					}
+				}
+			}
+		}
 	});
   
 	calendar.render();
